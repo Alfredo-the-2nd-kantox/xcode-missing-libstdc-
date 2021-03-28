8f664dad9b3bff177f0a7dbb83a72ec7e23da09e
Alfredo Roca <alfredo.roca@kantox.com>
2021-03-28 17:34:27 +0200

    Adapt install.sh for no-xcode installation
    This is what I used after upgrading Mohave to BigSur
    No xcode installation.

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ set -e
 
 sudo ln -s \
     $PWD/include/c++ \
-    /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/
+    /Library/Developer/CommandLineTools/SDKs/MacOSX11.1.sdk/usr/include/
 
 sudo ln -s \
     $PWD/lib/libstdc++.6.0.9.tbd \
     $PWD/lib/libstdc++.6.tbd \
     $PWD/lib/libstdc++.tbd \
-    /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/
+    /Library/Developer/CommandLineTools/SDKs/MacOSX11.1.sdk/usr/lib/
